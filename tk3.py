1842b16f3c772227ee863a4bd661e31db9b86e8b
Liveo123 <paul.livesey@googlemail.com>
2018-01-21 12:37:21 +0400

    Finished tk3 by adding positioning to LEFT for buttons.

--- tk3.py
+++ tk3.py
@@ -13,9 +13,9 @@ button2 = tk.Button(topFrame, text = 'button 2', bg='blue' )
 button3 = tk.Button(topFrame, text = 'button 3', fg='green' )
 button4 = tk.Button(bottomFrame, text = 'button 4', fg='purple' )
 
-button1.pack()
-button2.pack()
-button3.pack()
-button4.pack()
+button1.pack(side=tk.LEFT)
+button2.pack(side=tk.LEFT)
+button3.pack(side=tk.LEFT)
+button4.pack(side=tk.BOTTOM)
 
 root.mainloop()
